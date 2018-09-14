f07dd304b9b5c80236f6f77c84ca1da259b92cd4
mczyzj <mczyzj@gmail.com>
2018-09-15 00:46:47 +0200

    added dummy R file to upload indicate import functions from

---
+++ R/pestrPRAtemplates.R
@@ -0,0 +1,13 @@
+#' pestrPRAtemplates: A package for creating Express PRA Templates
+#'
+#' Based on the
+#' \href{https://www.eppo.int/RESOURCES/eppo_standards/pm5_pra}{EPPO PRA}.
+#'
+#' @importFrom magrittr %>%
+#' @importFrom rlang .data
+#' @importFrom stats setNames
+#' @import dplyr
+#' @docType package
+#' @name pestrPRAtemplates
+NULL
+utils::globalVariables(c("."))
