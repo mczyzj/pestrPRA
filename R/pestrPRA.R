8b36d145b20466bac04bc0544cdba1800cc8c036
mczyzj <mczyzj@gmail.com>
2018-09-24 12:13:47 +0200

    description update and final cleaning of functions

--- R/pestrPRA.R
+++ R/pestrPRA.R
@@ -23,35 +23,7 @@ eppo_invasive_pdf <- function(toc = FALSE, highlight = "default", ...){
                                         #latex_engine = 'xelatex',
                                         ...)
 
-#  old_opt <- getOption("bookdown.post.latex")
-#  options(bookdown.post.latex = fix_envs)
-#  on.exit(options(bookdown.post.late = old_opt))
-
   expressPRA
-
-}
-
-#' @inheritParams rmarkdown::html_document
-#' @param extra_dependencies,... Additional function arguments to pass to the
-#'        base R Markdown HTML output formatter
-#' @rdname eppoPRA
-#' @export
-eppo_invasive_html <- function(toc = FALSE, highlight = "default", ...){
-
-  expressPRA <- bookdown::html_document2(template = 'template.html',
-                                         toc = toc,
-                                         highlight = highlight, ...)
-                                         #keep_tex = TRUE,
-                                         #dev = 'cairo_pdf',
-                                         #latex_engine = 'xelatex',
-
-
-#  old_opt <- getOption("bookdown.post.latex")
-#  options(bookdown.post.latex = fix_envs)
-#  on.exit(options(bookdown.post.late = old_opt))
-
-  expressPRA
-
 }
 
 #' @inheritParams rmarkdown::pdf_document
@@ -70,30 +42,41 @@ eppo_pest_pdf <- function(toc = FALSE, highlight = "default", ...){
 
   expressPRA
 }
-#' @inheritParams rmarkdown::html_document
-#' @param extra_dependencies,... Additional function arguments to pass to the
-#'        base R Markdown HTML output formatter
-#' @rdname eppoPRA
-#' @export
-eppo_pest_html <- function(toc = FALSE, highlight = "default", ...){
-  expressPRA <- bookdown::html_document2(template = 'template.html',
-                                          toc = toc,
-                                          highlight = highlight,
-                                          #keep_tex = TRUE,
-                                          #dev = 'cairo_pdf',
-                                          #latex_engine = 'xelatex',
-                                          ...)
 
-  expressPRA
-}
-#fix_envs = function(x){
-#  beg_reg <- '^\\s*\\\\begin\\{.*\\}'
-#  end_reg <- '^\\s*\\\\end\\{.*\\}'
-#  i3 = if (length(i1 <- grep(beg_reg, x))) (i1 - 1)[grepl("^\\s*$", x[i1 - 1])]
-#
-#  i3 = c(i3,
-#         if (length(i2 <- grep(end_reg, x))) (i2 + 1)[grepl("^\\s*$", x[i2 + 1])]
-#  )
-#  if (length(i3)) x = x[-i3]
-#  x
-#}
+## In case of further difrentiation into separate functions for html and
+## pdf output, below there are skeletons for functions.
+
+## #' @inheritParams rmarkdown::html_document
+## #' @param extra_dependencies,... Additional function arguments to pass to the
+## #'        base R Markdown HTML output formatter
+## #' @rdname eppoPRA
+## #' @export
+## eppo_pest_html <- function(toc = FALSE, highlight = "default", ...){
+##   expressPRA <- bookdown::html_document2(template = 'template.html',
+##                                           toc = toc,
+##                                           highlight = highlight,
+##                                           #keep_tex = TRUE,
+##                                           #dev = 'cairo_pdf',
+##                                           #latex_engine = 'xelatex',
+##                                           ...)
+
+##   expressPRA
+## }
+
+## #' @inheritParams rmarkdown::html_document
+## #' @param extra_dependencies,... Additional function arguments to pass to the
+## #'        base R Markdown HTML output formatter
+## #' @rdname eppoPRA
+## #' @export
+## eppo_invasive_html <- function(toc = FALSE, highlight = "default", ...){
+
+##  expressPRA <- bookdown::html_document2(template = 'template.html',
+##                                         toc = toc,
+##                                         highlight = highlight, ...)
+##                                         #keep_tex = TRUE,
+##                                         #dev = 'cairo_pdf',
+##                                         #latex_engine = 'xelatex',
+
+##  expressPRA
+
+## }
---
+++ inst/rmarkdown/templates/eppo_pest_html/skeleton/abstract.md
@@ -0,0 +1,50 @@
+<!-- Please exchange instruction (text between asterisks) with proper informations  -->
+\begin{tcolorbox}[minipage,colback=Goldenrod,colframe=black,arc=1pt,outer arc=1pt,adjusted title= \textbf{Summary of the Express Pest Risk Analysis for \emph{*“pest name”*} }]
+\normalsize
+\textbf{PRA area:} *specify the PRA area being assessed*\newline
+\textbf{Describe the endangered area:} *(see question 14)*\newline
+\noindent\rule{\linewidth}{0.5pt}
+\textbf{Main conclusions:} \newline
+\textbf{Overall assessment of risk: } \tcbox[enhanced,frame style={opacity=0.25},interior style={opacity=0.5},size=tight,colframe=Bittersweet,colback=Bittersweet!70!white,shrink tight,extrude by=0.5mm,nobeforeafter,arc=2.5pt,outer arc=2.5pt]{*(Copy your answer from Q 16)*}.\newline\newline
+\textbf{Phytosanitary Measures:} *indicate whether the pest should be recommended for immediate action in the PRA area. Summarize your answer from Q 17.*\newline
+\small*Note: If the assessment shows that phytosanitary measures are not required for a country but there are indications that other EPPO countries are at higher risk, mention it.*\newline
+\noindent\rule{\linewidth}{0.5pt}
+  \begin{tcolorbox}[title=\textcolor{black}{\textbf{Phytosanitary risk for the endangered area:} },after skip=0pt,inherit height,arc=1pt,outer arc=1pt, colframe=Dandelion, colback=Yellow]
+*(Individual ratings for likelihood of entry and establishment, and for magnitude of spread and impact are provided in the document)*\newline
+    \begin{tcbraster}[raster columns=3,fonttitle=\bfseries,nobeforeafter,colback=Yellow,colframe=Bittersweet,arc=1pt,outer arc=1pt,boxrule=0.5pt,bottomrule=1pt,toprule=1pt,center title]
+      \begin{tcolorbox}[adjusted title=High,,halign=flush center]
+        \textbf{*.*}
+      \end{tcolorbox}\hfill
+      \begin{tcolorbox}[adjusted title=Moderate,halign=flush center]
+        \textbf{*.*}
+      \end{tcolorbox}\hfill
+      \begin{tcolorbox}[adjusted title=Low,halign=flush center]
+        \textbf{*.*}
+      \end{tcolorbox}
+    \end{tcbraster}
+  \end{tcolorbox}
+  \begin{tcolorbox}[title=\textcolor{black}{\textbf{Level of uncertainty of assessment:} },before skip=0pt,inherit height,arc=1pt,outer arc=1pt, colframe=Dandelion, colback=Yellow]
+*(see Q 18 for the justification of the rating. Individual ratings of uncertainty of entry, establishment, spread and impact are provided in the document)* \newline
+    \begin{tcbraster}[raster columns=3,fonttitle=\bfseries,nobeforeafter,colback=Yellow,colframe=Bittersweet,arc=1pt,outer arc=1pt,boxrule=0.5pt,bottomrule=1pt,toprule=1pt,center title]
+      \begin{tcolorbox}[adjusted title=High,,halign=flush center]
+        \textbf{*.*}
+      \end{tcolorbox}\hfill
+      \begin{tcolorbox}[adjusted title=Moderate,,halign=flush center]
+        \textbf{*.*}
+      \end{tcolorbox}\hfill
+      \begin{tcolorbox}[adjusted title=Low,halign=flush center]
+        \textbf{*.*}
+      \end{tcolorbox}
+    \end{tcbraster}  
+  \end{tcolorbox}
+\noindent\rule{\linewidth}{1pt}  
+\textbf{Other recommendations:
+\begin{itemize}
+\item *Inform EPPO or IPPC or EU.
+\item Inform industry, other stakeholders.
+\item State whether a detailed PRA is needed to reduce level of uncertainty (if so, state which parts of the PRA should be focused on).
+\item Specify if surveys are recommended to confirm the pest status.
+\item State what additional work/research could help making a decision.*
+\end{itemize}}
+
+\end{tcolorbox}
